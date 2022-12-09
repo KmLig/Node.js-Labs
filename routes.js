f2661f29b565317da6f3809b57cc49de06fee116
KmLig <phamxuankhiem1102@gmail.com>
2022-12-10 05:03:08 +0700

    modified routes.js

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
-
 const requestHandler = (req, res) => {
-    const url = req.url;
+  const url = req.url;
   const method = req.method;
   const userArray = ["An", "Binh"];
+  console.log(req.url, req.method);
 
   if (url === "/") {
     res.write("<html>");
@@ -28,7 +28,7 @@ const requestHandler = (req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const newUser = parsedBody.split("=")[1];
       userArray.push(newUser);
@@ -37,11 +37,17 @@ const requestHandler = (req, res) => {
       res.write("<head><title>Lab2_1</title></head>");
       userArray.map((user) => {
         res.write(`<li>${user}</li>`);
-      });      
+      });
       res.write("</html>");
       return res.end();
     });
   }
-} 
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hi from my Node.js server!</h1></body>");
+  res.write("</html>");
+  res.end();
+};
 
 module.exports.requestHandler = requestHandler;
