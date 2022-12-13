78581253eda0dcb386bff18862478d7f1e75179a
KmLig <phamxuankhiem1102@gmail.com>
2022-12-13 17:26:00 +0700

    converting HTML pages into Pug templates

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,25 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add product",
+    path: "/admin/add-product",
+  });
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   products.push({ title: req.body.title });
-  res.redirect('/');
+  res.redirect("/");
 });
 
 exports.routes = router;
